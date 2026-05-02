8392c93b6947ca78b626e0d80b00d92b64976344
Swayam <swayamg741@gmail.com>
2026-05-02 11:39:19 +0530

    frontend deployment fix

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://project-task-manager-production-56d6.up.railway.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
