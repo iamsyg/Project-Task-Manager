901b5db98e7eb1cf01d930834d3dd15a63f3e2d5
Swayam <swayamg741@gmail.com>
2026-05-02 12:04:19 +0530

    deployment fix

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://project-task-manager-production-56d6.up.railway.app/"
+        "https://project-task-manager-production-56d6.up.railway.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
